52fabfd2fdfaf7e7986fe9f41bfd299d6c60e103
Sneha <sneha.firodiya@pubmatic.com>
2022-04-28 18:39:15 +0530

    files added

---
+++ MyCocoapodLibrary/Classes/Person.h
@@ -0,0 +1,16 @@
+//
+//  Logger.h
+//  MyCocoapodLibrary
+//
+//  Created by Sneha on 05/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Logger : NSObject
+- (void)printLog ;
+@end
+
+NS_ASSUME_NONNULL_END
